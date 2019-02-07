00cbc9a247f11a62e59a012c1c35ebbaf0b0a073
heesu-na <hsra1000@gmail.com>
2019-02-07 18:31:59 +0900

    과제3 ID와 비밀번호 입력값에 따른 유효성 체크 추가

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 function check_input_len(val1, val2) {
     var len =  val1.length;
     var _val = document.getElementById('_'+val2);
+    
     if(len > 0) {
         _val.classList.add('value', 'focus_in');
     } else {
@@ -22,14 +23,15 @@ function focus_in(val1, val2) {
     } else {
         _val.classList.remove('focus_out');
     }
+
 }
 
 function focus_out(val) {
     var _val = document.getElementById('_'+val);
     var layout_val = document.getElementById(val); 
-
+    
     layout_val.classList.remove('focus_layout');
-    _val.classList.add('focus_out');
+    _val.classList.add('focus_out');    
 }
 
   function show_or_hide(e) {
@@ -46,11 +48,59 @@ function focus_out(val) {
   }
 
   function hide_checkbox() {
-    var id = document.getElementById('pwd');
-    var isFocus = id.classList.contains('focus_layout');
-    var len = id.value.length;
+    var pwd = document.getElementById('pwd');
+    var isFocus = pwd.classList.contains('focus_layout');
+    var len = pwd.value.length;
     if ((isFocus == false) && (len == 0)){
         var show = document.getElementById('show_check');
         show.classList.remove('show');
     }
   }
+
+  function submit_login() {
+    console.log('hihihi');
+      var id = document.getElementById('id');
+      var pwd = document.getElementById('pwd');
+      var layer = document.getElementById('layer2');
+      
+      if((id.value.length == 0) || (pwd.value.length == 0)) {
+          layer.classList.add('animation');
+      }
+  }
+
+  function point_cursor() {
+      var btn = document.getElementById('btn');
+      btn.classList.add('btn_cursor');
+  }
+
+  function check_validation_in(val) {
+      var extraInfo_val = document.getElementById(val+'_extraInfo');
+      extraInfo_val.classList.add('focus_extraInfo');
+      if(val == 'pwd'){
+          extraInfo_val.innerHTML = 'Type 6 characters or more';
+      }
+  }
+
+  function check_validation_out(val) {
+    var extraInfo_val = document.getElementById(val+'_extraInfo');
+    extraInfo_val.innerHTML = '';
+    extraInfo_val.classList.remove('focus_extraInfo');
+  if(val == 'id'){
+      var ch;
+      var id_val = document.getElementById(val);
+      var len = id_val.value.length;
+      if(len <= 2) {
+        extraInfo_val.classList.add('focus_extraInfo');
+        extraInfo_val.innerHTML = "Your username is probably longer than this. can you double check?";
+      } else {
+      for(i=0; i<len; i++){
+          ch = id_val.value.charAt(i);
+          if(!(ch>='0' && ch<='9') && !(ch>='a' && ch<='z') && !(ch>='A' && ch<='Z') && !(ch=='_')) {
+              extraInfo_val.classList.add('focus_extraInfo');
+              extraInfo_val.innerHTML = "use letters, numbers or '_'. (e.g. arya_starks)";
+              break;
+          }
+      } 
+    }
+  }
+}
