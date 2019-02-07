ad1c39b697d98bf286efdfe65358cb5831981392
heesu-na <hsra1000@gmail.com>
2019-02-07 15:17:41 +0900

    과제3 password show/hide를 위한 checkbox에 대해 이벤트 수정

--- index.js
+++ index.js
@@ -1,22 +1,56 @@
-  function check_input_len(val1, val2) {
-      var len =  val1.length;
-      var _val = document.querySelector('#_'+val2);
-      if(len > 0) {
-        _val.style.color="#ffbe2f";
-        _val.style.top="-33px";
-        _val.style.fontSize="15px";
-        _val.style.padding="0";
-        _val.style.transition="linear 0.15s";  
+//var idIndicator = document.getElementById('_id');
+//var pwdIndicator = document.getElementById('_pwd');
+
+function check_input_len(val1, val2) {
+    var len =  val1.length;
+    var _val = document.getElementById('_'+val2);
+    if(len > 0) {
+        _val.classList.add('value', 'focus_in');
+    } else {
+        _val.classList.remove('value', 'focus_in');
+    }
+}
+function focus_in(val1, val2) {
+    var len =  val1.length;
+    var layout_val = document.getElementById(val2); 
+    var _val = document.getElementById('_'+val2);
+    
+    layout_val.classList.add('focus_layout');
+    if(len > 0) {
+        _val.classList.remove('focus_out');
+        _val.classList.add('focus_in');
+    } else {
+        _val.classList.remove('focus_out');
+    }
+}
+
+function focus_out(val) {
+    var _val = document.getElementById('_'+val);
+    var layout_val = document.getElementById(val); 
+
+    layout_val.classList.remove('focus_layout');
+    _val.classList.add('focus_out');
+}
+
+  function show_or_hide(e) {
+      var pwd = document.getElementById('pwd');
+      if (e.target.checked==true){
+          pwd.type = "text";
       } else {
-        _val.style.color="darkgray";
-        _val.style.top="-8px";
-        _val.style.fontSize="18px";
-        _val.style.transition="linear 0.05s"; 
+          pwd.type = "password";
       }
   }
+  function show_checkbox() {
+    var show = document.getElementById('show_check');
+    show.classList.add('show');
+  }
 
-  function check_focus(val1) {
-      console.log("check");
-    var _val = document.querySelector('#_'+val1);
-    _val.style.color="darkgray";
+  function hide_checkbox() {
+    var id = document.getElementById('pwd');
+    var isFocus = id.classList.contains('focus_layout');
+    var len = id.value.length;
+    if ((isFocus == false) && (len == 0)){
+        var show = document.getElementById('show_check');
+        show.classList.remove('show');
+    }
   }
